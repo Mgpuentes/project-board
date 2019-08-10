87183284fc178320fdd50dccac99959529bd6b19
Matthew puentes <mpuent1@students.towson.edu>
2019-08-10 10:22:13 -0400

    Configured http request for index controller

--- ppm-tool/src/main/java/com/developersboard/ppmtool/controller/IndexController.java
+++ ppm-tool/src/main/java/com/developersboard/ppmtool/controller/IndexController.java
@@ -1,11 +1,15 @@
 package com.developersboard.ppmtool.controller;
 
+import com.developersboard.ppmtool.backend.persistance.domain.Project;
 import com.developersboard.ppmtool.backend.service.ProjectService;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 /**
@@ -19,6 +23,7 @@ public class IndexController {
 
     private final ProjectService projectService;
 
+    @Autowired
     public IndexController(ProjectService projectService) {
         this.projectService = projectService;
     }
@@ -26,7 +31,13 @@ public class IndexController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("projects", projectService.findAllProject());
+        model.addAttribute("projectAdded", new Project());
         return "hello";
     }
 
+    @PostMapping("/addingProject")
+    public String projectSubmit(@ModelAttribute("projectAdded") Project project) {
+        projectService.saveOrUpdateProject(project);
+        return "redirect:/";
+    }
 }
